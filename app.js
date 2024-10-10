06324d8658f43dc3ef90290d7d8a4871927dd143
Cade Holmes <holmesc16@gmail.com>
2024-10-10 17:29:22 -0500

    fix app entry

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
-require('dotenv').config();
 const { trainModel } = require('./models/trainer.js');
 
-console.log('Training mode enabled. Starting training...');
-
-trainModel()
-    .then(() => {
-        console.log('Model training complete.');
-        process.exit(0);
-    })
-    .catch(err => {
-        console.error('Error during model training:', err);
-        process.exit(1);
-    })
-    .finally(() => {
-        console.log('Model training completed.');
-    });
-
+(async () => {
+    try {
+        await trainModel();
+    } catch (error) {
+        console.error('Error during model training:', error);
+    }
+})();
--- models/trainer.js
+++ models/trainer.js
@@ -1,23 +1,20 @@
 const tf = require('@tensorflow/tfjs-node');
+const path = require('path');
+const { createDataset } = require('../controllers/imageController.js');
 
-// Load the pre-trained MobileNet model
 async function loadPretrainedModel() {
     const mobilenet = await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');
-    // Remove the top layers
     const truncatedModel = tf.model({
         inputs: mobilenet.inputs,
-        outputs: mobilenet.layers[mobilenet.layers.length - 4].output // Adjust based on the layer you want to truncate at
+        outputs: mobilenet.layers[mobilenet.layers.length - 4].output
     });
     return truncatedModel;
 }
 
 async function createModel() {
     const baseModel = await loadPretrainedModel();
-
-    // Freeze the base model layers
     baseModel.trainable = false;
 
-    // Add new layers on top of the base model
     const model = tf.sequential();
     model.add(baseModel);
     model.add(tf.layers.flatten());
@@ -25,7 +22,6 @@ async function createModel() {
     model.add(tf.layers.dropout({ rate: 0.5 }));
     model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
 
-    // Compile the model
     model.compile({
         optimizer: tf.train.adam(),
         loss: 'binaryCrossentropy',
@@ -37,21 +33,19 @@ async function createModel() {
 
 async function trainModel() {
     const model = await createModel();
-    const { dataset, totalSize } = await createDataset(16); // Adjust batch size as needed
+    const { dataset, totalSize } = await createDataset(16);
 
-    // Train the model
     await model.fitDataset(dataset, {
         epochs: 10,
         validationSplit: 0.2,
         callbacks: tf.callbacks.earlyStopping({ monitor: 'val_loss', patience: 5 })
     });
 
-    // Save the model
     const modelPath = path.join(__dirname, '..', 'trained_models', 'weldscanner_quality_model');
     await model.save(`file://${modelPath}`);
     console.log('Model trained and saved.');
 }
 
-trainModel().catch(err => {
-    console.error('Error during training:', err);
-});
+module.exports = {
+    trainModel
+};
