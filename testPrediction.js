97b5b4eb46cad31be8f78014c80142c04cd2465b
Cade Holmes <holmesc16@gmail.com>
2024-10-12 22:59:31 -0500

    more logging for predictions

--- testPrediction.js
+++ testPrediction.js
@@ -3,6 +3,7 @@ const FormData = require('form-data');
 const { S3Client, ListObjectsV2Command, GetObjectCommand } = require('@aws-sdk/client-s3');
 const s3Client = new S3Client({ region: 'us-west-1' });
 const bucket = 'weldscanner';
+const chalk = require('chalk');
 
 async function testPrediction(category) {
     try {
@@ -13,35 +14,43 @@ async function testPrediction(category) {
         
         const categoryImages = images.filter(image=> image.Key.includes(category))
         if (categoryImages.length === 0) {
-            console.error(`No images found for category: ${category}`);
+            console.error(chalk.red(`No images found for category: ${category}`));
             return;
         }
 
-        const randomImage = categoryImages[Math.floor(Math.random() * categoryImages.length)];
-        console.log('Random image: ', randomImage.Key);
+        for (let img of categoryImages) {
+            setTimeout(async () => {
+            const getObjectCommand = new GetObjectCommand({ Bucket: bucket, Key: img.Key });
+            const imageData = await s3Client.send(getObjectCommand);
         
-        const getObjectCommand = new GetObjectCommand({ Bucket: bucket, Key: randomImage.Key });
-        const imageData = await s3Client.send(getObjectCommand);
-        
-        const chunks = [];
-        for await (const chunk of imageData.Body) {
-            chunks.push(chunk);
-        }
-        const imageBuffer = Buffer.concat(chunks);
-
-        const formData = new FormData();
-        formData.append('image', imageBuffer, randomImage.Key);
-        formData.append('category', category);
-
-        const predictionResponse = await axios.post('http://localhost:3000/image', formData, {
-            headers: { 
-                ...formData.getHeaders()
+            const chunks = [];
+            for await (const chunk of imageData.Body) {
+                chunks.push(chunk);
+                const imageBuffer = Buffer.concat(chunks);
+                
+                const formData = new FormData();
+                formData.append('image', imageBuffer, img.Key);
+                formData.append('category', category);
+                
+                const predictionResponse = await axios.post('http://localhost:3000/image', formData, {
+                    headers: { 
+                        ...formData.getHeaders()
+                    }
+                });
+                const { category, result } = predictionResponse.data;
+                const expectedCategory = category;
+                const returnedCategory = category;
+                console.log(chalk.cyan.bold('Expected: '), chalk.yellow.bgBlack(expectedCategory));
+                console.log(chalk.yellow.bold('Returned: '), chalk.yellow.bgBlack(returnedCategory));
+                if (expectedCategory !== returnedCategory) {
+                    console.log(chalk.red.bold('Inaccurate Prediction'));
+                }
+                console.log(chalk.green.bold('Result: '), chalk.yellow.bgBlack(`${result}`));
             }
-        });
-
-        console.log('Prediction: ', predictionResponse.data);
+            }, 3000);
+        }
     } catch (error) {
-        console.error('Error during prediction:', error.response ? error.response.data : error.message);
+        console.error(chalk.red('Error during prediction:'), error.response ? error.response.data : error.message);
     }
 }
 
